072896230a64f1e16a1224f276ba972f3317b82f
Dmitry Sergeev <jitten@Dmitrys-MacBook-Air.local>
2025-07-13 20:02:11 +0300

    Fixed some stupid moves

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ def main():
     print("Reading PCAP file...")
     f_bars_720p_1 = "pcap/ST2110-20_720p_59_94_color_bars.pcap"
     f_bars_720p_2 = "pcap/ST2110-20_720p_59_94_color_bars_vendor_2.pcap"
-    f = open(f_bars_720p_2, "rb")
+    f_bars_1080i_1 = "pcap/ST2110-20_1080i_59_94_color_bars.pcap"
+    f = open(f_bars_1080i_1, "rb")
     pcap = dpkt.pcap.Reader(f)
     #given data
-    video_width = 1280
-    video_height = 720
-    scan = _PROGRESSIVE
+    video_width = 1920
+    video_height = 1080
+    scan = _INTERLACED
     depth = 10
     sampling = "YCrCb 4:2:2"
     directory = "test-images"
@@ -30,6 +31,7 @@ def main():
     image_buf = np.zeros((video_height, video_width, 3), dtype=np.uint16)
     packet_number = 0
     for _, buf in pcap:
+        packet_number += 1
         eth = dpkt.ethernet.Ethernet(buf)
         if not isinstance(eth.data, dpkt.ip.IP):
             continue
@@ -39,24 +41,22 @@ def main():
         udp = ip.data
         rtp = dpkt.rtp.RTP(udp.data)
         st = st2110_20.ST2110_20(rtp.data)
-        #processing ST-2110-20 payload
-        packet_number += 1
-        st_payload = st.payload
+        #processing ST-2110-20 header
         srd_field = st.srd_f
-        srd_len = st.srd_len
+        # srd_len = st.srd_len
+        srd_row = st.srd_row
+        srd_offset = st.srd_offset
+        #processing pgroups
+        st_payload = st.payload
         current_offsets = {}
-        current_len = 0
+        #Field Flag = 1 means second field/segment
+        if (scan == _INTERLACED and srd_field == 1):
+            srd_row = srd_row * 2 + 1
+        elif (scan == _INTERLACED and srd_field == 0):
+            srd_row *= 2
         for i in range(0, len(st_payload), pgroup_size):
             if i + pgroup_size > len(st_payload):
                 break
-            if current_len > srd_len:
-                break
-            #processing pgroups
-            srd_row = st.srd_row
-            #Field Flag = 1 means second field/segment
-            if (srd_field == 1):
-                srd_row += video_height // 2
-            srd_offset = st.srd_offset
             if srd_offset not in current_offsets:
                 current_offsets[srd_offset] = srd_offset
             pgroup = int.from_bytes(st_payload[i:i+pgroup_size])
@@ -79,7 +79,6 @@ def main():
                 pixel_comps[0]
             ]
             current_offsets[srd_offset] += pgroup_coverage
-            srd_len += pgroup_size * 8
         #end of frame or second field/segment
         if ((scan == _INTERLACED and rtp.m == 1 and srd_field == 1) or
             (scan == _PROGRESSIVE and rtp.m == 1)):
