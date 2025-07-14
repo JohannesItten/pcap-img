a4233fdac1162e7ffaca7dba90fab82b92bb14e5
Dmitry Sergeev <jitten@Dmitrys-MacBook-Air.local>
2025-07-14 22:55:11 +0300

    Better to read, before commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import dpkt
-import st2110_20
 import numpy as np
 import cv2
 import pgroup as pg
+from srd import SampleRowData, SampleRowDataSegment, SampleRowDataHeader
 
 _INTERLACED = 0
 _PROGRESSIVE = 1
@@ -12,26 +12,26 @@ def main():
     f_bars_720p_1 = "pcap/ST2110-20_720p_59_94_color_bars.pcap"
     f_bars_720p_2 = "pcap/ST2110-20_720p_59_94_color_bars_vendor_2.pcap"
     f_bars_1080i_1 = "pcap/ST2110-20_1080i_59_94_color_bars.pcap"
-    own_pcap_1 = "own-pcap/bm-2.pcap"
-    f = open(own_pcap_1, "rb")
+    own_pcap_1 = "own-pcap/1.pcap"
+    f = open(f_bars_720p_1, "rb")
     pcap = dpkt.pcap.Reader(f)
     #given data
-    video_width = 1920
-    video_height = 1080
+    video_width = 1280
+    video_height = 720
     scan = _PROGRESSIVE
     colorspace = "YCbCr"
     sampling = "4:2:2"
-    depth = 8
+    depth = 10
     directory = "test-images"
-    image_limit = 2
+    image_limit = 10
     saved_images_amount = 0
     #end of user data
     pgroup = pg.Pgroup(colorspace=colorspace,
                        sampling=sampling,
                        depth=depth)
-    image_buf = np.zeros((video_height, video_width, 3), dtype=np.uint8)
+    image_buf = np.zeros((video_height, video_width, 3), dtype=np.uint16)
     packet_number = 0
-    for _, buf in pcap:
+    for _, buf in pcap: 
         packet_number += 1
         eth = dpkt.ethernet.Ethernet(buf)
         if not isinstance(eth.data, dpkt.ip.IP):
@@ -41,43 +41,33 @@ def main():
             continue
         udp = ip.data
         rtp = dpkt.rtp.RTP(udp.data)
-        st = st2110_20.ST2110_20(rtp.data)
-        #processing ST-2110-20 header
-        srd_field = st.srd_f
-        srd_row = st.srd_row
-        srd_offset = st.srd_offset
-        #Field Flag = 1 means second field/segment
-        if (scan == _INTERLACED and srd_field == 1):
-            srd_row = srd_row * 2 + 1
-        elif (scan == _INTERLACED and srd_field == 0):
-            srd_row *= 2
-        #processing pgroups
-        st_payload = st.payload
-        current_offsets = {}
-        for i in range(0, len(st_payload), pgroup.size):
-            if i + pgroup.size > len(st_payload):
+        srd = SampleRowData(rtp.data)
+        for segment in srd.segments:
+            current_row = segment.row
+            current_offset = segment.offset
+            if (scan == _INTERLACED and segment.field == 1):
+                current_row = current_row * 2 + 1
+            elif (scan == _INTERLACED and segment.field == 0):
+                current_row *= 2
+            #process pgroups
+            for i in range(0, segment.length, pgroup.size):
+                pixels = pgroup.get_pixels(segment.data[i:i+pgroup.size])
+                for p in pixels:
+                    image_buf[current_row, current_offset] = p
+                    current_offset += 1
+            #end of frame or second field/segment
+            if ((scan == _INTERLACED and rtp.m == 1 and segment.field == 1) or
+                (scan == _PROGRESSIVE and rtp.m == 1)):
+                print("Last packet received, saving image...")
+                #TODO: figure out with OpenCV and sampling
+                image_buf = (image_buf / 1023 * 255).astype(np.uint8)  # Convert to 8-bit for display
+                converted_buf = cv2.cvtColor(image_buf, cv2.COLOR_YCrCb2BGR)
+                cv2.imwrite(f"{directory}/img-{packet_number}.png", converted_buf)
+                image_buf = np.zeros((video_height, video_width, 3), dtype=np.uint16)
+                saved_images_amount += 1
+            if (saved_images_amount >= image_limit):
+                print(f"Reached image limit, which is: {image_limit}")
                 break
-            if srd_offset not in current_offsets:
-                current_offsets[srd_offset] = srd_offset
-            #Pixel components
-            pixels = pgroup.get_pixels(st_payload[i:i+pgroup.size])
-            for p in pixels:
-                image_buf[srd_row, current_offsets[srd_offset]] = p
-                current_offsets[srd_offset] += 1
-        #end of frame or second field/segment
-        if ((scan == _INTERLACED and rtp.m == 1 and srd_field == 1) or
-            (scan == _PROGRESSIVE and rtp.m == 1)):
-            print("Last packet received, saving image...")
-            current_offsets = {}
-            #TODO: figure out with OpenCV and sampling
-            # image_buf = (image_buf / 1023 * 255).astype(np.uint8)  # Convert to 8-bit for display
-            converted_buf = cv2.cvtColor(image_buf, cv2.COLOR_YCrCb2BGR)
-            cv2.imwrite(f"{directory}/img-{packet_number}.png", converted_buf)
-            image_buf = np.zeros((video_height, video_width, 3), dtype=np.uint8)
-            saved_images_amount += 1
-        if (saved_images_amount >= image_limit):
-            print(f"Reached image limit, which is: {image_limit}")
-            break
 
 if __name__ == "__main__":
     main()
---
+++ srd.py
@@ -0,0 +1,71 @@
+#Sample Row Data (SRD) header fields
+#esn - Extended Sequence Number (16 bits)
+#srd_len - Sample Row Data Length (16 bits)
+#srd_f - Field Identification Flag (1 bit)
+#srd_row - Sample Row Data Number (15 bits)
+#srd_c - Continuation Flag (1 bit)
+#srd_offset - Sample Row Data Offset (15 bits)
+#... 6 bytes per 1 SRD Header
+
+
+#RTP packet shall not contain more, than 3 SRD Headers
+#1 SRD Header = 6 bytes
+_MAX_SRD_AMOUNT = 3
+_SRD_HEADER_SIZE = 6
+_ESN_SIZE = 2
+_HEADER_FLAG_SHIFT = 15
+
+class SampleRowDataHeader:
+    def __init__(self, data: bytes):
+        self.length = int.from_bytes(data[:2], byteorder="big")
+        row = int.from_bytes(data[2:4], byteorder="big")
+        offset = int.from_bytes(data[4:6], byteorder="big")
+        offset_row_mask = (1 << _HEADER_FLAG_SHIFT) - 1
+        self.field = row >> _HEADER_FLAG_SHIFT
+        self.continuos = offset >> _HEADER_FLAG_SHIFT
+        self.row = row & offset_row_mask
+        self.offset = offset & offset_row_mask
+
+
+class SampleRowDataSegment:
+    def __init__(self, header: SampleRowDataHeader, data: bytes):
+        self.length = header.length
+        self.field = header.field
+        self.row = header.row
+        self.offset = header.offset
+        self.data = data
+
+
+class SampleRowData:
+    def __init__(self, data: bytes):
+        self.esn = int.from_bytes(data[:_ESN_SIZE], byteorder="big")
+        self.headers = self._get_headers(data[_ESN_SIZE:_MAX_SRD_AMOUNT * _SRD_HEADER_SIZE])
+        header_len = _ESN_SIZE + (len(self.headers) * _SRD_HEADER_SIZE)
+        segments_data = data[header_len:]
+        self.segments = self._get_segments(segments_data=segments_data)
+
+
+    def _get_headers(self, headers_data: bytes):
+        headers = []
+        for i in range(0, len(headers_data), _SRD_HEADER_SIZE):
+            head = SampleRowDataHeader(data=headers_data)
+            headers.append(head)
+            if head.continuos == 0:
+                break
+        return headers
+    
+    #returns each SRD segment with its corresponded header
+    def _get_segments(self, segments_data: bytes):
+        segments = []
+        prev_srd_len = 0
+        for header in self.headers:
+            # TODO: should ask about that someone
+            # cause ST-2110-20 leaves me with some questions
+            srd_len = header.length
+            if srd_len == 0:
+                break
+            seg = SampleRowDataSegment(header, segments_data[prev_srd_len:srd_len])
+            segments.append(seg)
+        return segments
+
+            
